da7e3692651a0ef2336106f7570b6270627ac4d7
gilbert-kiana <gilbertintabo@gmail.com>
2022-02-24 16:07:36 +0300

    For the app I added a mongo database connection to the local host Created Schema for user details

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//connect to Mongo
+mongoose
+  .connect("mongodb://localhost/user")
+  .then(() => console.log("mongodb connected..."))
+  .catch((err) => console.log(err));
 
 //EJS THE VIEW ENGINE
 app.use(expressLayouts);
