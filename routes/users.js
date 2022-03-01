74592d632f053b6d08d82026b3f3126221bfb56f
gilbert-kiana <gilbertintabo@gmail.com>
2022-03-01 10:56:11 +0300

    added a router to the login page and logout page

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 //login page
 router.get("/login", (req, res) => res.render("login"));
@@ -37,25 +38,43 @@ router.post("/register", (req, res) => {
   } else {
     User.findone(
       { email: email }.then((user) => {
-        if (user) {
-          errors.push({ msg: "Email already exists" });
-          res.render("render", {
-            errors,
-            name,
-            email,
-            password,
-            password2,
-          });
-        } else {
-          const newUser = new User({
-            name,
-            email,
-            password,
-          });
-        }
+        User.findOne({ email: email }).then((user) => {
+          if (user) {
+            errors.push({ msg: "Email already exists" });
+            res.render("register", {
+              errors,
+              name,
+              email,
+              password,
+              password2,
+            });
+          } else {
+            const newUser = new User({
+              name,
+              email,
+              password,
+            });
+          }
+        });
       })
     );
   }
+});
+
+// Login
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })(req, res, next);
+});
+
+// Logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
