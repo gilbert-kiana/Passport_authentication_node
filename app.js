da0e1f50da46d6f023de1e7a15bd496f0bb09ac5
gilbert-kiana <gilbertintabo@gmail.com>
2022-03-01 10:54:48 +0300

    added passportjs ,set up its middleware.Initialised global variables to display error messages.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+require("./config/passport")(passport);
 
 //connect to Mongo
 mongoose
@@ -27,7 +30,19 @@ app.use(
   })
 );
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
+
+// Global variables
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
