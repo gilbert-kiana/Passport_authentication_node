89f9d815239fc40ca3cc4eca19852f594ae05ff4
gilbert-kiana <gilbertintabo@gmail.com>
2022-03-29 12:49:58 +0300

    error fix

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ router.post("/register", (req, res) => {
 // Login
 router.post("/login", (req, res, next) => {
   passport.authenticate("local", {
-    successRedirect: "./socketio/server",
+    successRedirect: "/dashboard",
     failureRedirect: "/users/login",
     failureFlash: true,
   })(req, res, next);
